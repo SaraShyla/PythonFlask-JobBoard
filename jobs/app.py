545d6a7e4feff94d71edbb47ba4e5918e596e5a0
Sara Shyls <sara00221122@gmail.com>
2022-02-07 15:13:46 +0530

    commit

--- jobs/app.py
+++ jobs/app.py
@@ -7,10 +7,9 @@ app = Flask(__name__)
 
 
 @app.route('/job/<job_id>')
-def job():
-    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?','[job_id]', single =True)
-
-    return render_template('job.html',jobs = job)
+def job(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?',[job_id], single =True)
+    return render_template('job.html',job = job)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
